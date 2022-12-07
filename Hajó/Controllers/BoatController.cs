0560bc5b1c304901015629d77c9e920fd9e3364a
Kisking <kisfaludy.kinga@stud.uni-corvinus.hu>
2022-12-07 13:03:35 +0100

    Add project files.

---
+++ Hajó/Controllers/BoatController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hajó.Controllers
+{
+    //[Route("api/[controller]")]
+    //[ApiController]
+    public class BoatController : ControllerBase
+    {
+        [HttpGet]
+        [Route("hajo/kerdesek")]
+        public IActionResult függvénynév()
+        {
+            Models.HajosContext hajosContext = new Models.HajosContext();
+            var lista = from x in hajosContext.Questions
+                        select x.Question1;
+            return Ok(lista);
+        }
+
+        [HttpGet]
+        [Route("hajo/kerdesek/{id}")]
+        public IActionResult f2(int id)
+        {
+            Models.HajosContext hajosContext = new Models.HajosContext();
+            var lisa = from x in hajosContext.Questions
+                       where x.QuestionId == id
+                       select x;
+
+            var lisa2 = hajosContext.Questions.Where(x => x.QuestionId == id);
+
+            return Ok(lisa.FirstOrDefault());
+        }
+
+        [HttpGet]
+        [Route("questions/{sorszám}")]
+        public ActionResult M2(int sorszám)
+        {
+            Models.HajosContext hajosContext = new Models.HajosContext();
+            var kérdés = (from x in hajosContext.Questions
+                          where x.QuestionId == sorszám
+                          select x).FirstOrDefault();
+
+            if (kérdés == null) return BadRequest("Nincs ilyen sorszámú kérdés");
+
+            return new JsonResult(kérdés);
+        }
+    }
+}
